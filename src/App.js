c7de64ed900f289378d74715d8f64e42baf0a4a8
bagja <barjafaskan9@gmail.com>
2023-11-26 19:43:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./component/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./component/Register";
+import Homepage from "./component/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <Router>
+      
+      <Routes>
+        {/*   Di dalam Routes yang kita akan tambahlan endpoit - endpoint nya  */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/homepage"  element={<Homepage />} />   
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/component/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+
+    //Logic dari web yang menggunakan java scrip
+
+  return (
+    <div>Ini adalah halaman login</div>
+  )
+}
+
+export default Login
